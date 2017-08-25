7854c114a8f74ab10e3b359ce65f61bfaf4ec7ca
AndrewTestma <425780724@qq.com>
2017-08-25 17:20:26 +0800

    修改TestBaseCase类

--- src/main/java/utils/TestBaseCase.java
+++ src/main/java/utils/TestBaseCase.java
@@ -2,12 +2,12 @@ package utils;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 /**
  * Created by Administrator on 2017/8/25 0025.
@@ -34,5 +34,22 @@ public class TestBaseCase {
     @AfterClass
     public void endStart(){
         log.exit();
+    }
+    @AfterMethod
+    public void ExtentResult(ITestResult result){
+        if(result.getStatus()==ITestResult.FAILURE){
+            extentTest.log(LogStatus.FAIL,result.getThrowable());
+        }else if(result.getStatus()==ITestResult.SKIP){
+            extentTest.log(LogStatus.SKIP,result.getThrowable());
+        }else{
+            extentTest.log(LogStatus.PASS,"Pass");
+        }
+        extentReports.endTest(extentTest);
+        extentReports.flush();
+    }
+    @AfterSuite
+    public void closeExtentReport(){
+        extentReports.flush();
+        extentReports.close();
     }
 }
