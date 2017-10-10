260b534bcea1d9a8675fdc2b45e55825e8c5ccb4
AndrewTestma <425780724@qq.com>
2017-10-10 09:56:00 +0800

    修改driver启动设置

--- src/main/java/utils/TestBaseCase.java
+++ src/main/java/utils/TestBaseCase.java
@@ -19,14 +19,18 @@ public class TestBaseCase {
     public static ExtentTest extentTest;
     public static String reportLocation="ExtentReport.html";
     public static int types;
+    public DriverManager manager = new DriverManager();
     public  Logger log=LogManager.getLogger(this.getClass().getName());
 
     @BeforeSuite
-    public void initializationExtentReport(){
+    @Parameters({"browserType","driverPath"})
+    public void initializationExtentReport(String browserType,String driverPath){
         /*initialization ExtentReport*/
         extentReports=new ExtentReports(reportLocation,true);
         extentReports
                 .addSystemInfo("Author","Andrew");
+        manager.setupDriver(browserType,driverPath);
+        driver=manager.getDriver();
     }
     @BeforeTest
     @Parameters({"type"})
@@ -35,14 +39,16 @@ public class TestBaseCase {
         log.entry();
         log.info("【开始执行测试】");
         this.types=type;
-        System.setProperty("webdriver.chrome.driver","./src/main/resources/driver/Chromedriver.exe");
-        driver=new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver","./src/main/resources/driver/Chromedriver.exe");
+        //driver=new ChromeDriver();
+        this.driver.manage().window().maximize();
     }
     @AfterTest
     public void endStart(){
         log.exit();
-        driver.close();
-        driver.quit();
+        //driver.close();
+        //driver.quit();
+        manager.quitDriver();
     }
     @AfterMethod
     public void ExtentResult(ITestResult result){
